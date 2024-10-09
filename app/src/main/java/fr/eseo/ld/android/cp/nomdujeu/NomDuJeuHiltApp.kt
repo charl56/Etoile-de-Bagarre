709dce362ece7ace8aca11df9f6edeba302e7827
charl56 <charles.pichou56@gmail.com>
2024-10-09 22:59:35 +0200

    Mise en place LibGDX avec Jetpack Compose

--- app/src/main/java/fr/eseo/ld/android/cp/nomdujeu/NomDuJeuHiltApp.kt
+++
@@ -1,8 +0,0 @@
-package fr.eseo.ld.android.cp.nomdujeu
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class NomDuJeuHiltApp : Application() {
-}
--- app/src/main/java/fr/eseo/ld/android/cp/nomdujeu/ui/screens/WaitingScreen.kt
+++
@@ -1,8 +0,0 @@
-package fr.eseo.ld.android.cp.nomdujeu.ui.screens
-
-import androidx.navigation.NavController
-import fr.eseo.ld.android.cp.nomdujeu.viewmodels.AuthenticationViewModel
-
-fun WaitingScreen(navController: NavController, authenticationViewModel: AuthenticationViewModel) {
-
-}
