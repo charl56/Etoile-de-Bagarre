49b36be342b19b598aa810e3928aa3d1760057e0
charl56 <charles.pichou56@gmail.com>
2024-10-09 11:34:26 +0200

    first commit

---
+++ app/src/main/java/fr/eseo/ld/android/cp/nomdujeu/NomDuJeuHiltApp.kt
@@ -0,0 +1,8 @@
+package fr.eseo.ld.android.cp.nomdujeu
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NomDuJeuHiltApp : Application() {
+}
---
+++ app/src/main/java/fr/eseo/ld/android/cp/nomdujeu/ui/screens/EndGameScreen.kt
@@ -0,0 +1,8 @@
+package fr.eseo.ld.android.cp.nomdujeu.ui.screens
+
+import androidx.navigation.NavController
+import fr.eseo.ld.android.cp.nomdujeu.viewmodels.AuthenticationViewModel
+
+fun EndGameScreen(navController: NavController, authenticationViewModel: AuthenticationViewModel) {
+
+}
