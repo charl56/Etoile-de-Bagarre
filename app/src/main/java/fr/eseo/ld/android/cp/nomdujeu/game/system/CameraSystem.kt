471dd606f82551d69608882cfb427e97fead69ff
Thomas Carreau <thomas.carreau@reseau.eseo.fr>
2024-11-04 14:37:32 +0100

    feat : camera now stop at map border

--- app/src/main/java/fr/eseo/ld/android/cp/nomdujeu/game/system/CameraSystem.kt
+++ app/src/main/java/fr/eseo/ld/android/cp/nomdujeu/game/system/CameraSystem.kt
@@ -1,5 +1,7 @@
 package fr.eseo.ld.android.cp.nomdujeu.game.system
 
+import com.badlogic.gdx.scenes.scene2d.Event
+import com.badlogic.gdx.scenes.scene2d.EventListener
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.github.quillraven.fleks.AllOf
 import com.github.quillraven.fleks.ComponentMapper
@@ -7,22 +9,40 @@ import com.github.quillraven.fleks.Entity
 import com.github.quillraven.fleks.IteratingSystem
 import fr.eseo.ld.android.cp.nomdujeu.game.component.ImageComponent
 import fr.eseo.ld.android.cp.nomdujeu.game.component.PlayerComponent
+import fr.eseo.ld.android.cp.nomdujeu.game.event.MapChangeEvent
+import ktx.tiled.height
+import ktx.tiled.width
 
 @AllOf([PlayerComponent::class, ImageComponent::class])
 class CameraSystem(
     private val imageCmps : ComponentMapper<ImageComponent>,
     stage : Stage
-) : IteratingSystem() { 
+) : EventListener, IteratingSystem() {
 
+    private var maxWidth = 0f
+    private var maxHeight = 0f
     private val camera = stage.camera
 
     override fun onTickEntity(entity: Entity) {
         with(imageCmps[entity]) {
+
+            val viewWith = camera.viewportWidth / 2
+            val viewHeight = camera.viewportHeight / 2
+
             camera.position.set(
-                image.x,
-                image.y,
+                image.x.coerceIn(viewWith, maxWidth - viewWith),
+                image.y.coerceIn(viewHeight, maxHeight - viewHeight),
                 camera.position.z
             )
         }
+    }
+
+    override fun handle(event: Event?): Boolean {
+        if (event is MapChangeEvent){
+            maxWidth = event.map.width.toFloat()
+            maxHeight = event.map.height.toFloat()
+            return true
+        }
+        return false
     }
 }
